a99e392ca4fa27e61d6a3d3d23649c8f049dfde2
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-22 04:16:46 -0600

    just see how far you can get ignoring the bad data

--- dexter/logs2trajs.py
+++ dexter/logs2trajs.py
@@ -157,21 +157,37 @@ class LogReader(Player):
 def process_logs(log_jsons: dict[str, tuple[str, str]]) -> list[Trajectory]:
     trajs = []
     total = 0
+    num_errors = 0
     for i, (tag, (_, log)) in enumerate(log_jsons.items()):
         print(f"progress: {round(100 * i / len(log_jsons), ndigits=2)}%", end="\r")
-        player1 = LogReader(battle_format=battle_format, log_level=51, accept_open_team_sheet=True)
-        player2 = LogReader(battle_format=battle_format, log_level=51, accept_open_team_sheet=True)
-        result1 = asyncio.run(player1.follow_log(tag, log, "p1"))
-        result2 = asyncio.run(player2.follow_log(tag, log, "p2"))
-        if result1 is not None:
-            states1, actions1 = result1
-            total += len(states1)
-            trajs += [Trajectory(obs=states1, acts=actions1, infos=None, terminal=True)]
-        if result2 is not None:
-            states2, actions2 = result2
-            total += len(states2)
-            trajs += [Trajectory(obs=states2, acts=actions2, infos=None, terminal=True)]
-    print(f"prepared {len(trajs)} trajectories with {total} total state-action pairs")
+        try:
+            player1 = LogReader(
+                battle_format=battle_format, log_level=51, accept_open_team_sheet=True
+            )
+            player2 = LogReader(
+                battle_format=battle_format, log_level=51, accept_open_team_sheet=True
+            )
+            result1 = asyncio.run(player1.follow_log(tag, log, "p1"))
+            result2 = asyncio.run(player2.follow_log(tag, log, "p2"))
+            if result1 is not None:
+                states1, actions1 = result1
+                total += len(states1)
+                trajs += [Trajectory(obs=states1, acts=actions1, infos=None, terminal=True)]
+            if result2 is not None:
+                states2, actions2 = result2
+                total += len(states2)
+                trajs += [Trajectory(obs=states2, acts=actions2, infos=None, terminal=True)]
+        except KeyboardInterrupt:
+            raise
+        except SystemExit:
+            raise
+        except:
+            num_errors += 1
+    print(
+        f"prepared {len(trajs)} trajectories "
+        f"with {total} total state-action pairs "
+        f"(and {num_errors} games thrown away)"
+    )
     return trajs
 
 
