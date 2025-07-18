6228b7d59cb4a9a1de63fe011767b78447b42312
Cameron Angliss <cameronangliss99@gmail.com>
2025-07-17 22:02:27 -0500

    cleanup, use short description

--- vgc_bench/scrape_data.py
+++ vgc_bench/scrape_data.py
@@ -1,11 +1,8 @@
 import json
 import os
 import re
-import warnings
 
 import requests
-from sentence_transformers import SentenceTransformer
-from sklearn.decomposition import PCA
 
 
 def update_desc_embeddings(url: str, file: str, extras: dict[str, dict[str, str]] = {}):
@@ -18,7 +15,11 @@ def update_desc_embeddings(url: str, file: str, extras: dict[str, dict[str, str]
         js_literal = js_text[i:-1]
         json_text = re.sub(r"([{,])([a-zA-Z0-9_]+)(:)", r'\1"\2"\3', js_literal)
         file += "on"
-    dex = {k: v["desc"] for k, v in {**extras, **json.loads(json_text)}.items() if "desc" in v}
+    dex = {
+        k: v["shortDesc"]
+        for k, v in {**extras, **json.loads(json_text)}.items()
+        if "shortDesc" in v
+    }
     with open(f"data/{file}", "w") as f:
         json.dump(dex, f)
 
@@ -27,17 +28,21 @@ if __name__ == "__main__":
     if not os.path.exists("data"):
         os.mkdir("data")
     update_desc_embeddings(
-        "https://play.pokemonshowdown.com/data", "abilities.js", extras={"null": {"desc": "null"}}
+        "https://play.pokemonshowdown.com/data",
+        "abilities.js",
+        extras={"null": {"shortDesc": "null"}},
     )
     update_desc_embeddings(
         "https://play.pokemonshowdown.com/data",
         "items.js",
         extras={
-            "null": {"desc": "null"},
-            "": {"desc": "empty"},
-            "unknown_item": {"desc": "unknown item"},
+            "null": {"shortDesc": "null"},
+            "": {"shortDesc": "empty"},
+            "unknown_item": {"shortDesc": "unknown item"},
         },
     )
     update_desc_embeddings(
-        "https://play.pokemonshowdown.com/data", "moves.js", extras={"no move": {"desc": "no move"}}
+        "https://play.pokemonshowdown.com/data",
+        "moves.js",
+        extras={"no move": {"shortDesc": "no move"}},
     )
